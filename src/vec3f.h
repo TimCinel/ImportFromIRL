79cec108597fc04cd519dad0ea6e547550f6c6f0
Tim Cinel <email@timcinel.com>
2012-10-07 19:44:16 +1100

    Turned vec3f into a class - now has a constructor and some operator overloads.

--- src/vec3f.h
+++ src/vec3f.h
@@ -1,8 +1,22 @@
 #ifndef VEC3F_H
 #define VEC3F_H
 
-typedef struct t_vec3f {
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+class vec3f {
+public:
+	vec3f() : x(0.0), y(0.0), z(0.0) {}
+	vec3f(float x, float y, float z) : x(x), y(y), z(z) {}
 	float x, y, z;
-} vec3f;
+
+	bool operator==(float &rhs) {
+		return rhs == (float)(*this);
+	}
+
+	operator float() {
+		return sqrt(pow(this->x,2) + pow(this->y,2) + pow(this->z,2));
+	}
+};
 
 #endif
